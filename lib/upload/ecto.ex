a6c39337be938bcb37883978840aafab62afb4ba
Ray Zane <ray@promptworks.com>
2017-10-26 02:20:20 -0400

    This will be simpler

--- lib/upload/ecto.ex
+++ lib/upload/ecto.ex
@@ -1,32 +1,38 @@
 defmodule Upload.Ecto do
+  @doc """
+  Casts an upload in the params under the given key, uploads it, and assigns it to the field.
+  """
   def cast_upload(%Ecto.Changeset{params: params} = changeset, field, opts \\ []) do
-    uploader = Keyword.get(opts, :uploader, Upload)
+    cast = Keyword.get(opts, :with, &Upload.cast/2)
 
     if file = Map.get(params, Atom.to_string(field)) do
-      case uploader.cast(file, Keyword.delete(opts, :uploader)) do
+      case cast.(file, Keyword.delete(opts, :with)) do
         {:ok, upload} ->
-          put_upload(changeset, field, upload, opts)
+          put_upload(changeset, field, upload)
 
         {:error, message} when is_binary(message) ->
-          add_error(changeset, field, message)
+          Ecto.Changeset.add_error(changeset, field, message)
       end
     else
       changeset
     end
   end
 
-  def put_upload(changeset, field, %Upload{status: :pending} = upload, opts \\ []) do
-    uploader = Keyword.get(opts, :uploader, Upload)
+  @doc """
+  Add the Upload's key to the changeset for the given field.
 
-    case uploader.transfer(upload) do
+  If the file hasn't been uploaded yet, it will be.
+  """
+  def put_upload(changeset, field, %Upload{status: :pending} = upload) do
+    case Upload.transfer(upload) do
       {:ok, upload} ->
-        put_upload(changeset, field, upload, opts)
+        put_upload(changeset, field, upload)
 
       {:error, _} ->
-        add_error(changeset, field, "failed to upload")
+        Ecto.Changeset.add_error(changeset, field, "failed to upload")
     end
   end
-  def put_upload(changeset, field, %Upload{status: :completed, key: key}, _opts) do
+  def put_upload(changeset, field, %Upload{status: :completed, key: key}) do
     Ecto.Changeset.put_change(changeset, field, key)
   end
 end
