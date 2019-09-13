3756aa5bb188b5b683ed5a08535dafa08b295f0f
Ray Zane <raymondzane@gmail.com>
2019-09-13 17:18:59 -0400

    Rebuilding...

--- lib/upload.ex
+++ lib/upload.ex
@@ -3,8 +3,16 @@ defmodule Upload do
   An opinionated file uploader.
   """
 
+  defstruct [:path, :content_type, :filename]
+
+  defmodule Uploader do
+    @callback store() :: FileStore.t()
+  end
+
   defmacro __using__(_) do
     quote do
+      @behaviour Uploader
+
       use Ecto.Schema
       import Ecto.Changeset
 
@@ -28,8 +36,34 @@ defmodule Upload do
     end
   end
 
-  def cast_upload(changeset, key) do
+  def cast_upload(changeset, field, uploader) do
+    case Map.get(changeset.params, field) do
+      %Plug.Upload{path: path, content_type: content_type, filename: filename} ->
+        upload = %Upload{path: path, content_type: content_type, filename: filename}
+        put_upload(changeset, field, upload)
+
+      %Upload{} = upload ->
+        put_upload(changeset, field, upload)
+
+      _ ->
+        changeset
+    end
+  end
+
+  def put_upload(changeset, field, %Upload{} = upload) do
+    store = %FileStore{} #TODO: get it from the changeset based on field name
+    key = Ecto.UUID.generate()
+
     changeset
-    |> Ecto.Changeset.cast_embed(key)
+    |> cast_embed(field)
+    |> prepare_changes(fn ->
+      case FileStore.copy(store, upload.path, key) do
+        :ok ->
+          cast_embed(changeset, field)
+
+        :error ->
+          add_error(changeset, field, "unable to upload")
+      end
+    end)
   end
 end
--- test/upload_test.exs
+++ test/upload_test.exs
@@ -21,6 +21,11 @@ defmodule UploadTest do
 
   defmodule Person.Avatar do
     use Upload
+
+    @impl true
+    def store do
+      # Application.get_env()
+    end
   end
 
   setup do
@@ -29,7 +34,10 @@ defmodule UploadTest do
 
   test "saves a file" do
     assert {:ok, _} = start_supervised(FileStore.Adapters.Test)
-    changeset = Person.changeset(%Person{}, %{avatar: %{key: "foo", filename: "bar"}})
+    changeset = Person.changeset(
+      %Person{},
+      %{avatar: %Plug.Upload{filename: "bar", path: "/foo/bar", content_type: "adf"}}
+    )
 
     assert {:ok, person} = Repo.insert(changeset)
     assert person.avatar.key == "foo"
