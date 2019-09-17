bc361977098c4f997560930aa17b56f30bcb51d4
Ray Zane <raymondzane@gmail.com>
2019-09-17 12:07:16 -0400

    Use signed keys

--- example/lib/example_web/views/user_view.ex
+++ example/lib/example_web/views/user_view.ex
@@ -2,15 +2,10 @@ defmodule ExampleWeb.UserView do
   use ExampleWeb, :view
 
   def blob_path(blob) do
-    "/upload/blobs/#{blob.key}"
+    Upload.Endpoint.blob_path(blob)
   end
 
   defp variant_path(blob, transforms) do
-    variation_key =
-      transforms
-      |> Map.new(fn {k, v} -> {to_string(k), v} end)
-      |> Upload.Key.sign(:variation)
-
-    "/upload/variant/#{blob.key}/#{variation_key}"
+    Upload.Endpoint.variant_path(blob, transforms)
   end
 end
--- lib/upload/endpoint.ex
+++ lib/upload/endpoint.ex
@@ -3,49 +3,93 @@ defmodule Upload.Endpoint do
 
   require Logger
 
-  plug :match
-  plug :dispatch
+  alias Upload.Key
+  alias Upload.Blob
+  alias Upload.Config
+  alias Upload.Variant
+  alias FileStore.Adapters.Disk
 
-  # TODO: There are issues here...
+  plug(:match)
+  plug(:dispatch)
+
+  # TODO: Figure out the actual URL this is mounted at?
+  @spec blob_path(Plug.Conn.t(), Blob.t() | Key.t(), Keyword.t()) :: binary()
+  def blob_path(conn, blob_or_key, query \\ [])
+
+  def blob_path(conn, %Blob{key: key}, query) do
+    blob_path(conn, key, query)
+  end
+
+  def blob_path(_conn, key, query) when is_binary(key) do
+    signed_blob_key = Key.sign(key, :blob)
+    query_string = URI.encode_query(query)
+    "/upload/blobs/#{signed_blob_key}?#{query_string}"
+  end
+
+  # TODO: Figure out the actual URL this is mounted at?
+  @spec variant_path(Plug.Conn.t(), Blob.t() | Key.t(), Keyword.t(), Keyword.t()) :: binary()
+  def variant_path(conn, blob_or_key, transforms, query \\ [])
+
+  def variant_path(conn, %Blob{key: key}, transforms, query) do
+    variant_path(conn, key, transforms, query)
+  end
+
+  def variant_path(_conn, key, transforms, query) when is_binary(key) do
+    query_string = URI.encode_query(query)
+    signed_blob_key = Key.sign(key, :blob)
+
+    variation_key =
+      transforms
+      |> Map.new(fn {k, v} -> {to_string(k), v} end)
+      |> Key.sign(:variation)
+
+    "/upload/variants/#{signed_blob_key}/#{variation_key}?#{query_string}"
+  end
+
+  # TODO: Support filename
+  # TODO: Use an encoded/signed key?
   get "/disk/*key" do
     key = Enum.join(key, "/")
-    store = Upload.Config.file_store()
+    store = Config.file_store()
 
-    case FileStore.Adapters.Disk.get_path(store, key) do
+    case Disk.get_path(store, key) do
       {:ok, path} ->
         send_file(conn, 200, path)
 
-      _ ->
+      _error ->
         send_resp(conn, 404, "Not Found")
     end
   end
 
-  # TODO: Signed key?
-  # TODO: Redirect with pure plug?
-  get "/blobs/:key" do
-    store = Upload.Config.file_store()
-
-    case FileStore.get_signed_url(store, key) do
-      {:ok, url} ->
-        redirect(conn, url)
+  # TODO: Support filename
+  # TODO: Disposition query parameter
+  # TODO: Rails uses ID for blobs instead of key. Why?
+  get "/blobs/:signed_blob_key/*filename" do
+    with {:ok, blob_key} <- Key.verify(signed_blob_key, :blob),
+         {:ok, url} <- Upload.get_signed_url(blob_key) do
+      redirect(conn, url)
+    else
+      :error ->
+        send_resp(conn, 400, "Bad Request")
 
       {:error, _} ->
         send_resp(conn, 400, "Bad Request")
     end
   end
 
-  get "/variant/:key/:variation_key" do
-    blob = %Upload.Blob{key: key}
-    store = Upload.Config.file_store()
-
-    with {:ok, variant} <- Upload.Variant.decode(blob, variation_key),
-         {:ok, variant} <- Upload.Variant.process(variant),
-         {:ok, url} <- FileStore.get_signed_url(store, variant.key) do
+  # TODO: Support filename.
+  # TODO: Rails uses ID for blobs instead of key. Why?
+  # TODO: Consider removing `Variant.decode` and just `Key.verify` here.
+  get "/variants/:signed_blob_key/:variation_key/*filename" do
+    with {:ok, blob_key} <- Key.verify(signed_blob_key, :blob),
+         {:ok, variant} <- Variant.decode(blob_key, variation_key),
+         {:ok, variant} <- Variant.process(variant),
+         {:ok, url} <- Upload.get_signed_url(variant.key) do
       redirect(conn, url)
     else
       :error ->
-        Logger.error("Invalid key")
-        send_resp(conn, 404, "Not Found")
+        Logger.error("Failed to send variant")
+        send_resp(conn, 400, "Bad Request")
 
       {:error, reason} ->
         Logger.error("Failed to send variant (reason: #{reason})")
