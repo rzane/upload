ad8fb0fb50b36f61ec7c91a080dbf0b8bd6477bb
Ray Zane <raymondzane@gmail.com>
2019-09-17 00:01:27 -0400

    Fix failing test

--- test/upload/transformer_test.exs
+++ test/upload/transformer_test.exs
@@ -5,9 +5,10 @@ defmodule Upload.TransformerTest do
   @png_path "test/fixtures/test.png"
 
   test "transform/2" do
-    assert {:ok, path} = Transformer.transform(@png_path, %{"resize" => "50x50"})
-    assert File.regular?(path)
-    assert get_dimensions(path) == {50, 50}
+    assert {:ok, dest} = Plug.Upload.random_file("upload")
+    assert :ok = Transformer.transform(@png_path, dest, %{"resize" => "50x50"})
+    assert File.regular?(dest)
+    assert get_dimensions(dest) == {50, 50}
   end
 
   defp get_dimensions(path) do
