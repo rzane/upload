5d85f0d42d637682a7df04d83afdd40f1bcd83a0
Ray Zane <raymondzane@gmail.com>
2019-09-17 00:12:42 -0400

    Check for mogrify

--- lib/upload/transformer.ex
+++ lib/upload/transformer.ex
@@ -1,28 +1,35 @@
-defmodule Upload.Transformer do
-  # TODO: Check for Mogrify
-  # TODO: If the list of transforms is empty, the transformer should short circuit.
-  # TODO: Handle errors
+if Code.ensure_compiled?(Mogrify) do
+  defmodule Upload.Transformer do
+    @spec transform(Path.t(), Path.t(), map()) :: :ok | {:error, term()}
+    def transform(source, destination, transforms) do
+      transforms
+      |> Enum.reduce(Mogrify.open(source), &do_transform/2)
+      |> Mogrify.save(path: destination)
 
-  @spec transform(Path.t(), Path.t(), map()) :: :ok | {:error, term()}
-  def transform(source, destination, transforms) do
-    transforms
-    |> Enum.reduce(Mogrify.open(source), &do_transform/2)
-    |> Mogrify.save(path: destination)
+      :ok
+    rescue
+      error -> {:error, error}
+    end
 
-    :ok
+    defp do_transform({"resize", param}, image) do
+      Mogrify.resize(image, param)
+    end
+
+    defp do_transform({"resize_to_fill", param}, image) do
+      Mogrify.resize_to_fill(image, param)
+    end
+
+    defp do_transform({"resize_to_limit", param}, image) do
+      Mogrify.resize_to_fill(image, param)
+    end
+
+    defp do_transform(_transformation, image), do: image
   end
-
-  defp do_transform({"resize", param}, image) do
-    Mogrify.resize(image, param)
+else
+  defmodule Upload.Transformer do
+    @spec transform(Path.t(), Path.t(), map()) :: :ok | {:error, term()}
+    def transform(_source, _destination, _transforms) do
+      raise "Mogrify is not installed"
+    end
   end
-
-  defp do_transform({"resize_to_fill", param}, image) do
-    Mogrify.resize_to_fill(image, param)
-  end
-
-  defp do_transform({"resize_to_limit", param}, image) do
-    Mogrify.resize_to_fill(image, param)
-  end
-
-  defp do_transform(_transformation, image), do: image
 end
