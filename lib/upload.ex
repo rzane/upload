655eda1b0bb5a9527873479a7091620cdb3196a1
Ray Zane <raymondzane@gmail.com>
2019-09-17 12:06:09 -0400

    Allow URL helpers to take just a key

--- lib/upload.ex
+++ lib/upload.ex
@@ -6,13 +6,17 @@ defmodule Upload do
   alias Upload.Config
   alias Upload.Blob
 
-  @spec get_public_url(Blob.t(), Keyword.t()) :: binary()
-  def get_public_url(%Blob{key: key}, opts \\ []) do
+  @spec get_public_url(Key.t() | Blob.t(), Keyword.t()) :: binary()
+  def get_public_url(blob_or_key, opts \\ [])
+  def get_public_url(%Blob{key: key}, opts), do: get_public_url(key, opts)
+  def get_public_url(key, opts) when is_binary(key) do
     FileStore.get_public_url(Config.file_store(), key, opts)
   end
 
-  @spec get_signed_url(Blob.t(), Keyword.t()) :: {:ok, binary()} | {:error, term()}
-  def get_signed_url(%Blob{key: key}, opts \\ []) do
+  @spec get_signed_url(Key.t() | Blob.t(), Keyword.t()) :: {:ok, binary()} | {:error, term()}
+  def get_signed_url(blob_or_key, opts \\ [])
+  def get_signed_url(%Blob{key: key}, opts), do: get_signed_url(key, opts)
+  def get_signed_url(key, opts) when is_binary(key) do
     FileStore.get_signed_url(Config.file_store(), key, opts)
   end
 end
