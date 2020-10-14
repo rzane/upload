4dafc5ec364e9f62f0fba443076e97c2a2a9cf24
Ray Zane <raymondzane@gmail.com>
2020-10-13 21:10:20 -0400

    Add some logging

--- lib/upload/multi.ex
+++ lib/upload/multi.ex
@@ -4,15 +4,14 @@ defmodule Upload.Multi do
 
   alias Upload.Blob
   alias Upload.Storage
+  alias Upload.Utils
 
   def upload(multi, name, blob_name) do
     Multi.run(
       multi,
       {name, blob_name},
       fn _repo, %{^name => %{^blob_name => blob}} ->
-        blob
-        |> do_upload()
-        |> to_result()
+        do_upload(blob)
       end
     )
   end
@@ -21,17 +20,31 @@ defmodule Upload.Multi do
 
   def purge(multi, name, %Blob{key: key}) do
     Multi.run(multi, name, fn _, _ ->
-      key
-      |> Storage.delete()
-      |> to_result()
+      case Storage.delete(key) do
+        :ok ->
+          Utils.log(:info, "Deleted file from key: #{key}")
+          {:ok, nil}
+
+        {:error, reason} ->
+          Utils.log(:error, "Failed to delete file from key: #{key}")
+          {:error, reason}
+      end
     end)
   end
 
-  defp do_upload(nil), do: :ok
-  defp do_upload(%NotLoaded{}), do: :ok
-  defp do_upload(%Blob{path: nil}), do: :ok
-  defp do_upload(%Blob{path: path, key: key}), do: Storage.upload(path, key)
+  defp do_upload(nil), do: {:ok, nil}
+  defp do_upload(%NotLoaded{}), do: {:ok, nil}
+  defp do_upload(%Blob{path: nil}), do: {:ok, nil}
 
-  defp to_result(:ok), do: {:ok, nil}
-  defp to_result(other), do: other
+  defp do_upload(%Blob{path: path, key: key, checksum: checksum}) do
+    case Storage.upload(path, key) do
+      :ok ->
+        Utils.log(:info, "Uploaded file to key: #{key} (checksum: #{checksum})")
+        {:ok, nil}
+
+      {:error, reason} ->
+        Utils.log(:error, "Failed to load file to key: #{key} (reason: #{inspect(reason)})")
+        {:error, reason}
+    end
+  end
 end
--- test/upload/multi_test.exs
+++ test/upload/multi_test.exs
@@ -9,7 +9,7 @@ defmodule Upload.MultiTest do
 
   test "upload/3" do
     changeset = change_person(%{avatar: @upload})
-    assert {:ok, %{person: person}} = insert_person(changeset)
+    assert {:ok, %{person: person}} = upload_person(changeset)
     assert person.avatar_id
     assert person.avatar
     assert person.avatar.key in list_uploaded_keys()
@@ -17,29 +17,43 @@ defmodule Upload.MultiTest do
 
   test "upload/3 when avatar is not provided" do
     changeset = change_person(%{})
-    assert {:ok, %{person: person}} = insert_person(changeset)
+    assert {:ok, %{person: person}} = upload_person(changeset)
     refute person.avatar_id
   end
 
   test "purge/3" do
     changeset = change_person(%{avatar: @upload})
-    assert {:ok, %{person: person}} = insert_person(changeset)
+
+    assert {:ok, %{person: person}} = upload_person(changeset)
     assert person.avatar.key in list_uploaded_keys()
 
-    assert {:ok, _} =
-             Ecto.Multi.new()
-             |> Ecto.Multi.delete(:person, person)
-             |> Upload.Multi.purge(:avatar, person.avatar)
-             |> Repo.transaction()
-
+    assert {:ok, _} = purge_person(person)
     refute person.avatar.key in list_uploaded_keys()
   end
 
-  defp insert_person(changeset) do
+  test "purge/3 ignores nil" do
+    person = create_person(%{avatar: nil})
+    assert {:ok, _} = purge_person(person)
+  end
+
+  defp purge_person(person) do
+    Ecto.Multi.new()
+    |> Ecto.Multi.delete(:person, person)
+    |> Upload.Multi.purge(:avatar, person.avatar)
+    |> Repo.transaction()
+  end
+
+  defp upload_person(changeset) do
     Ecto.Multi.new()
     |> Ecto.Multi.insert(:person, changeset)
     |> Upload.Multi.upload(:person, :avatar)
     |> Repo.transaction()
+  end
+
+  defp create_person(attrs) do
+    attrs
+    |> change_person()
+    |> Repo.insert!()
   end
 
   defp change_person(attrs) do
