b817d081fa1a1994607795deffa70a0c0133d7c7
Ray Zane <raymondzane@gmail.com>
2020-10-14 22:35:25 -0400

    Process a variant

--- lib/upload/variant.ex
+++ lib/upload/variant.ex
@@ -1,10 +1,79 @@
 defmodule Upload.Variant do
-  alias Upload.Blob
   alias Upload.Token
+  alias Upload.Storage
+  alias Upload.Transformer
+  alias Upload.Utils
 
-  def generate_key(%Blob{key: key}, transforms) do
+  def generate_key(blob_key, transforms) do
     transform_key = Token.sign(transforms, :variant)
-    "variants/#{key}/#{hexdigest(transform_key)}"
+    "variants/#{blob_key}/#{hexdigest(transform_key)}"
+  end
+
+  def process(blob_key, transforms) do
+    variant_key = generate_key(blob_key, transforms)
+
+    with :error <- stat(variant_key),
+         {:ok, blob_path} <- tempfile(),
+         :ok <- download(blob_key, blob_path),
+         {:ok, variant_path} <- tempfile(),
+         :ok <- transform(blob_path, variant_path, transforms),
+         :ok <- cleanup(blob_path),
+         :ok <- upload(variant_path, variant_key),
+         :ok <- cleanup(variant_path),
+         do: {:ok, variant_key}
+  end
+
+  defp stat(key) do
+    case Storage.stat(key) do
+      {:ok, _} ->
+        Utils.log(:debug, "Check if file exists at key: #{key} (yes)")
+        {:ok, key}
+
+      {:error, _} ->
+        Utils.log(:debug, "Check if file exists at key: #{key} (no)")
+        :error
+    end
+  end
+
+  defp download(key, dest) do
+    case Storage.download(key, dest) do
+      :ok ->
+        Utils.log(:info, "Downloaded file from key: #{key}")
+        :ok
+
+      {:error, reason} ->
+        Utils.log(:error, "Failed to download file from key: #{key}")
+        {:error, {:download, reason}}
+    end
+  end
+
+  defp transform(source, dest, transforms) do
+    case Transformer.transform(source, dest, transforms) do
+      :ok -> :ok
+      {:error, reason} -> {:error, {:transform, reason}}
+    end
+  end
+
+  defp upload(path, key) do
+    case Storage.upload(path, key) do
+      :ok -> :ok
+      {:error, reason} -> {:error, {:upload, reason}}
+    end
+  end
+
+  defp cleanup(file) do
+    case File.rm(file) do
+      :ok -> :ok
+      {:error, reason} -> {:error, {:cleanup, reason}}
+    end
+  end
+
+  defp tempfile do
+    case Plug.Upload.random_file("upload") do
+      {:ok, tmp} -> {:ok, tmp}
+      {reason, _, _} -> {:error, {:tempfile, reason}}
+      {reason, _} -> {:error, {:tempfile, reason}}
+    end
   end
 
   defp hexdigest(data) do
--- test/upload/variant_test.exs
+++ test/upload/variant_test.exs
@@ -1,18 +1,38 @@
 defmodule Upload.VariantTest do
   use ExUnit.Case
 
-  alias Upload.Blob
   alias Upload.Variant
+  alias Upload.Analyzer.Image
+  alias FileStore.Adapters.Memory
 
   describe "generate_key/2" do
     test "generates a deterministic key to be used for storage" do
-      blob = %Blob{key: "abc"}
-      key = Variant.generate_key(blob, resize: "200x200")
+      key = Variant.generate_key("abc", resize: "200x200")
       assert key =~ ~r|^variants/abc/[a-z0-9]{64}$|
 
       for _ <- 1..10 do
-        assert Variant.generate_key(blob, resize: "200x200") == key
+        assert Variant.generate_key("abc", resize: "200x200") == key
       end
+    end
+  end
+
+  describe "process/2" do
+    @blob_key "abc"
+    @path Path.expand("../fixtures/racecar.jpg", __DIR__)
+
+    setup do
+      start_supervised!(Memory)
+      [store: FileStore.new(adapter: Memory)]
+    end
+
+    test "transforms an image", %{store: store} do
+      assert :ok = FileStore.upload(store, @path, @blob_key)
+      assert {:ok, variant_key} = Variant.process(@blob_key, resize: "10x7")
+      assert {:ok, _} = FileStore.stat(store, variant_key)
+      assert {:ok, tmp} = Plug.Upload.random_file("upload_test")
+      assert :ok = FileStore.download(store, variant_key, tmp)
+      assert {:ok, %{width: 10, height: 7}} = Image.get_metadata(tmp)
+      assert :ok = File.rm(tmp)
     end
   end
 end
