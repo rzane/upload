39e891a1525893f6d4ba86130b725f4a8a7f92cd
Ray Zane <raymondzane@gmail.com>
2020-10-14 20:30:49 -0400

    Remove intermediate function

--- lib/upload/key.ex
+++ lib/upload/key.ex
@@ -4,11 +4,8 @@ defmodule Upload.Key do
   @base36_alphabet '0123456789abcdefghijklmnopqrstuvwxyz'
 
   @spec generate() :: t()
-  def generate, do: random_base36(28)
-
-  # Pretty much a blatant ripoff of ActiveSupport's SecureRandom#base36
-  defp random_base36(length) do
-    length
+  def generate() do
+    28
     |> :crypto.strong_rand_bytes()
     |> :binary.bin_to_list()
     |> Enum.map_join(fn byte -> byte |> rem(64) |> base36() end)
