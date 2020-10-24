7b341d030b22154d0bd95e8524cd3781a2582801
Ray Zane <raymondzane@gmail.com>
2020-10-23 20:05:09 -0400

    Expand possible usages of Upload.Multi

--- lib/upload/multi.ex
+++ lib/upload/multi.ex
@@ -1,33 +1,51 @@
 defmodule Upload.Multi do
-  alias Ecto.Multi
-  alias Ecto.Association.NotLoaded
-
   alias Upload.Blob
   alias Upload.Storage
 
-  def upload(multi, name, blob_name) do
-    Multi.run(
-      multi,
-      {name, blob_name},
-      fn _repo, %{^name => %{^blob_name => blob}} ->
-        do_upload(blob)
-      end
-    )
+  alias Ecto.Multi
+  alias Ecto.Association.NotLoaded
+
+  @doc """
+  Upload a blob to storage.
+  """
+  @spec upload(Multi.t(), Multi.name(), Blob.t()) :: Multi.t()
+  def upload(multi, name, %Blob{key: key, path: path} = blob)
+      when is_binary(key) and is_binary(path) do
+    Multi.run(multi, name, fn _repo, _ctx -> do_upload(blob) end)
   end
 
-  def purge(multi, _name, nil), do: multi
-
-  def purge(multi, name, %Blob{key: key}) do
-    Multi.run(multi, name, fn _, _ ->
-      with :ok <- Storage.delete(key), do: {:ok, nil}
-    end)
+  @spec upload(Multi.t(), Multi.name(), Multi.fun(Blob.t())) :: Multi.t()
+  def upload(multi, name, fun) when is_function(fun) do
+    Multi.run(multi, name, fn _repo, ctx -> ctx |> fun.() |> do_upload() end)
   end
 
   defp do_upload(nil), do: {:ok, nil}
-  defp do_upload(%NotLoaded{}), do: {:ok, nil}
-  defp do_upload(%Blob{path: nil}), do: {:ok, nil}
+  defp do_upload(%NotLoaded{} = blob), do: {:ok, blob}
+  defp do_upload(%Blob{path: nil} = blob), do: {:ok, blob}
 
-  defp do_upload(%Blob{path: path, key: key}) do
-    with :ok <- Storage.upload(path, key), do: {:ok, nil}
+  defp do_upload(%Blob{path: path, key: key} = blob) when is_binary(key) do
+    with :ok <- Storage.upload(path, key),
+         do: {:ok, blob}
+  end
+
+  @doc """
+  Remove a blob and it's variants from storage.
+  """
+  @spec purge(Multi.t(), Multi.name(), Blob.t()) :: Multi.t()
+  def purge(multi, name, %Blob{key: key} = blob) when is_binary(key) do
+    Multi.run(multi, name, fn _repo, _ctx -> do_purge(blob) end)
+  end
+
+  @spec purge(Multi.t(), Multi.name(), Multi.fun(Blob.t())) :: Multi.t()
+  def purge(multi, name, fun) when is_function(fun) do
+    Multi.run(multi, name, fn _repo, ctx -> ctx |> fun.() |> do_purge() end)
+  end
+
+  defp do_purge(nil), do: {:ok, nil}
+
+  defp do_purge(%Blob{key: key} = blob) when is_binary(key) do
+    with :ok <- Storage.delete_all(prefix: "variants/#{key}/"),
+         :ok <- Storage.delete(key),
+         do: {:ok, blob}
   end
 end
