91b350c26fb680499d72eb0d44c4895ffec54d42
Ray Zane <raymondzane@gmail.com>
2020-11-15 00:13:12 -0500

    Fix the blob test

--- test/upload/blob_test.exs
+++ test/upload/blob_test.exs
@@ -2,58 +2,48 @@ defmodule Upload.BlobTest do
   use Upload.DataCase, async: true
   alias Upload.Blob
 
-  @path "test/fixtures/test.txt"
-  @upload %Plug.Upload{path: @path, filename: "racecar.jpg"}
+  @attributes %{
+    path: "test/fixtures/test.txt",
+    key: "abcdef",
+    filename: "text.txt",
+    content_type: "text/plain",
+    byte_size: 9,
+    checksum: "blah"
+  }
 
-  describe "from_path/1" do
-    test "builds a changeset" do
-      changeset = Blob.from_path(@path)
-      assert changeset.changes.key
-      assert changeset.changes.path
-      assert changeset.changes.filename
-      assert changeset.changes.content_type
-      assert changeset.changes.byte_size
-      assert changeset.changes.checksum
-    end
+  @errors %{
+    byte_size: ["can't be blank"],
+    checksum: ["can't be blank"],
+    filename: ["can't be blank"],
+    content_type: ["can't be blank"]
+  }
+
+  test "is valid with required attributes" do
+    changeset = Blob.changeset(%Blob{}, @attributes)
+    assert changeset.valid?
+    assert changeset.errors == []
   end
 
-  describe "from_plug/1" do
-    test "builds a changeset" do
-      changeset = Blob.from_plug(@upload)
-      assert changeset.changes.key
-      assert changeset.changes.path
-      assert changeset.changes.filename
-      assert changeset.changes.content_type
-      assert changeset.changes.byte_size
-      assert changeset.changes.checksum
-    end
+  test "is invalid when missing required attributes" do
+    changeset = Blob.changeset(%Blob{}, %{})
+    assert errors_on(changeset) == @errors
   end
 
-  describe "changeset/2" do
-    @attributes %{
-      path: @path,
-      key: "abcdef",
-      filename: "text.txt",
-      content_type: "text/plain",
-      byte_size: 9,
-      checksum: "blah"
-    }
+  test "generates a key" do
+    changeset = Blob.changeset(%Blob{}, %{})
+    assert changeset.changes.key
+  end
 
-    @errors %{
-      byte_size: ["can't be blank"],
-      checksum: ["can't be blank"],
-      filename: ["can't be blank"]
-    }
+  test "does not generate a key when explicitly provided" do
+    key = "abc"
+    changeset = Blob.changeset(%Blob{}, %{key: key})
+    assert changeset.changes.key == key
+  end
 
-    test "is valid with required attributes" do
-      changeset = Blob.changeset(%Blob{}, @attributes)
-      assert changeset.valid?
-      assert changeset.errors == []
-    end
-
-    test "is invalid when missing required attributes" do
-      changeset = Blob.changeset(%Blob{}, %{})
-      assert errors_on(changeset) == @errors
-    end
+  test "does not generate a key for an existing record" do
+    key = "abc"
+    changeset = Blob.changeset(%Blob{key: key}, %{})
+    assert changeset.changes == %{}
+    assert changeset.data.key == key
   end
 end
